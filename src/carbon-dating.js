3935948ebad717c78c9803853f4e9f7529309d7d
rubiaqute <rubiaqute@gmail.com>
2021-09-21 00:34:17 +0400

    complete some tasks

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,8 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function dateSample(sampleActivity) {
+if ((typeof sampleActivity !='string')   || (Boolean(+sampleActivity) ==false)||(+sampleActivity<0)||(+sampleActivity>15)||(+sampleActivity==0))  {
+  return false;
+  } else return Math.ceil((Math.log(MODERN_ACTIVITY/ +sampleActivity))/ (0.693/HALF_LIFE_PERIOD));
 }
