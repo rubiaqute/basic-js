90f5d28a7beb9799d64faf709b65fda92d823e23
rubiaqute <rubiaqute@gmail.com>
2021-09-21 23:18:40 +0400

    transform array done

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,33 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error ("'arr' parameter must be an instance of the Array!")
+  let newarr=[];
+  let check=0;
+  let flag=0;
+  for (let i=0;i<arr.length; i++){
+      
+      if (arr[i]=='--double-next'& (i+1<arr.length)) {newarr.push(arr[i+1]);}
+      //if (arr[i+1]==undefined) newarr.splice(i+1,1);
+      else {
+        if (arr[i]=='--discard-prev' & i!=0) {newarr.splice(i-1,1);}         
+        
+            else{ if (arr[i]=='--double-prev'& i!=0&arr[i-2]!='--discard-next') {newarr.push(newarr[i-1]);}
+                  else{  if (arr[i-1]!='--discard-next'&arr[i]!='--discard-next'&arr[i]!='--double-prev'&arr[i]!='--discard-prev'&arr[i]!='--double-next')newarr.push(arr[i]);
+      
+                     }
+            }
+          }
+        }
+            
+                 
+      
+      
+        
+      
+  
+  
+
+  return newarr;
 }
