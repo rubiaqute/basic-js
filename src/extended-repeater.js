d5f64aa466bb547e85d5a7c1d6f5c4e12a44c4fa
rubiaqute <rubiaqute@gmail.com>
2021-09-28 16:31:34 +0400

    extended-repeater done

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,24 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+  if (typeof options.separator!="string") options.separator="+";
+  if (typeof options.addition=="undefined") options.addition="";
+  if (typeof options.additionRepeatTimes=="underfined") options.additionRepeatTimes=0;
+  if (typeof options.additionSeparator=="undefined") options.additionSeparator="|";
+  if (typeof options.repeatTimes=="underfind") options.repeatTimes=0;
+  let adds=options.addition;
+  for (let j=0;j<options.additionRepeatTimes-1;j++){
+    adds=adds+options.additionSeparator+options.addition;
+  }
+ let result=str+adds;
+ 
+ 
+
+  for (let i=0;i<options.repeatTimes-1;i++){
+ result = result+options.separator+str+adds;
+  }
+  
+return result;
+  
 }
