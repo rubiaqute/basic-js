d7b632a9b6689d91a8cd46768313fcff4dcc1746
rubiaqute <rubiaqute@gmail.com>
2021-10-04 23:56:02 +0400

    feat: st-file-names done

--- src/st-file-names.js
+++ src/st-file-names.js
@@ -15,7 +15,48 @@ import { NotImplementedError } from '../extensions/index.js';
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-export default function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function renameFiles(names) {
+  let sort=[];
+  let k;
+  let results = [];
+ for (let i=0; i<names.length; i++){
+   k=0;
+   
+   for (let j=0;j<i;j++){
+     if (names[i]==names[j]) k=k+1;
+   }
+   if(k>0){
+   sort[i] = names[i] + "(" + `${k}` + ")";
+   while (results.includes(sort[i]))
+    { k=k+1;
+      sort[i] = names[i] + "(" + `${k}` + ")";
+      console.log(sort[i])
+    }
+     names[i]=names[i] +"(" + `${k}` + ")";
+  }
+   
+   
+   
+   results.push(names[i]);
+   console.log(results);
+   
+
+ }
+//    sort[i] = names.filter(item => item==names[i]);
+//    if (sort[i].length > 1){
+//      let k=1;
+//      for (let j=0; j<i; j++){
+//       if (names[j]==names[i]) { k=k+1};
+//      }
+//      if(k>1){
+//        names[i]=names[i] + "(" + `${k-1}` + ")";
+//        while
+//        results.push(names[i])
+//      } else results.push(names[i]);
+      
+
+//    } else results.push(names[i])
+//    console.log(results);
+//  }
+ return results;
 }
